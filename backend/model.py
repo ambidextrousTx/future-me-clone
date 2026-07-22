aec80195305a51972fe81b4596122f4813f2746f
Ravi Sinha <ravisinha@my.unt.edu>
2026-07-22 18:58:44 -0500

    Add Sqlalchemy model

---
+++ backend/model.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from sqlalchemy import CheckConstraint, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Email(Base):
+    __tablename__ = "emails"
+    __table_args__ = (
+        CheckConstraint(
+            "status IN ('pending', 'sent', 'failed')", name="status_check"
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    recipient_email: Mapped[str] = mapped_column(Text, nullable=False)
+    subject: Mapped[str] = mapped_column(Text, nullable=False)
+    body_html: Mapped[str] = mapped_column(Text, nullable=False)
+    send_date: Mapped[datetime] = mapped_column(nullable=False)
+    status: Mapped[str] = mapped_column(Text, nullable=False, default="pending")
+    created_at: Mapped[datetime] = mapped_column(
+        nullable=False, server_default=func.now()
+    )
+    sent_at: Mapped[datetime | None] = mapped_column(default=None)
+    send_attempts: Mapped[int] = mapped_column(nullable=False, default=0)
+    last_error: Mapped[str | None] = mapped_column(Text, default=None)
